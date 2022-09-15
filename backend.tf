29e6a757969e3ba99593f9a763312fcee0ef502a
Yelizaveta Ivanchenko <yelizaveta.ivanchenko@gmx.com>
2022-09-15 12:39:50 +0200

    using new folder for S3

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "ta-terraform-tfstates-041308375526"
-    key    = "sprint1/week2/training-terraform/terraform.tfstates"
+    key    = "sprint1/week2/training-terraform/terraform-backend/terraform.tfstates"
     dynamodb_table = "terraform-lock"
   } 
 }
